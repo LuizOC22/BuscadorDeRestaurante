4218b478c01812244c73bad87a48813966f90d23
LuizOC22 <107076186+LuizOC22@users.noreply.github.com>
2025-09-15 16:25:26 -0300

    Initial commit

---
+++ src/test/java/com/example/BuscadorDeRestaurantes/BuscadorDeRestaurantesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BuscadorDeRestaurantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuscadorDeRestaurantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
